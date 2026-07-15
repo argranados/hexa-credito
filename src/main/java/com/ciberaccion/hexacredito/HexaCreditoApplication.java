0ce8f98cce34362268c887915760547f0311b158
Ciberaccion <argranados@yahoo.com>
2026-07-15 14:44:48 -0600

    add project structure

---
+++ src/main/java/com/ciberaccion/hexacredito/HexaCreditoApplication.java
@@ -0,0 +1,13 @@
+package com.ciberaccion.hexacredito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexaCreditoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexaCreditoApplication.class, args);
+	}
+
+}
