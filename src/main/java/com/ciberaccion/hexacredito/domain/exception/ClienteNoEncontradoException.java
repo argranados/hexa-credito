c40893b3d5449a95411ddce8eb49f02fb7926fb9
Ciberaccion <argranados@yahoo.com>
2026-07-21 14:07:56 -0600

    fix bug wrong message in cliente exception

--- src/main/java/com/ciberaccion/hexacredito/domain/exception/ClienteNoEncontradoException.java
+++ src/main/java/com/ciberaccion/hexacredito/domain/exception/ClienteNoEncontradoException.java
@@ -2,6 +2,6 @@ package com.ciberaccion.hexacredito.domain.exception;
 
 public class ClienteNoEncontradoException extends RuntimeException{
     public ClienteNoEncontradoException(Long id) {
-        super("No se encontró la solicitud de crédito con id: " + id);
+        super("No se encontró el cliente con id: " + id);
     }
 }
